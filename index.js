064042055478571b9475e069b76b3402cab1dc21
Willigers <ronowilligers@gmail.com>
2024-07-14 11:27:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+const findMatching = (arr, driver) => {
+  return arr.filter((item) => item.toLowerCase() === driver.toLowerCase());
+};
+
+const fuzzyMatch = (arr, searchString) => {
+  return arr.filter((item) =>
+    item.toLowerCase().startsWith(searchString.toLowerCase())
+  );
+};
+
+const matchName = (arr, name) => {
+  return arr.filter((item) => item && item.name === name);
+};
